bb0ebe8c331f8b40d4b11fffafaa46a11a7f1060
Liubou <alekseevna0201@gmail.com>
2018-09-05 20:09:19 +0400

    Add some tests for boolean, number, string, null, undefined

--- first.test.js
+++ first.test.js
@@ -1,5 +1,34 @@
+/*
 describe('String methods', () => {
   it('String', () => {
-    expect('abc'.toLocaleLowerCase()).toBe(ABC);
+    const str = 'abc';
+    expect(str.toLocaleUpperCase()).toBe('ABC');
+  });
+});
+
+describe('Number methods', () => {
+  it('Number', () => {
+    const n = 120;
+    expect(n * 2).toBe(240);
+  });
+});
+
+describe('Boolean methods', () => {
+  it('Boolean', () => {
+    expect(NaN == NaN).toBe(false);
+  });
+});
+
+describe('Null methods', () => {
+  it('null', () => {
+    const n = null;
+    expect(n * 1).toBe(0);
+  });
+});*/
+
+describe('Undefined', () => {
+  it('undefined', () => {
+    const x = undefined;
+    expect(x == null).toBe(true);
   });
 });
