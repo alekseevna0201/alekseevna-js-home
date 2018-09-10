dbc04a8e0b531502676dcb359f81338da52faa0f
Liubou <alekseevna0201@gmail.com>
2018-09-10 11:54:18 +0400

    Add code for task2

--- lesson-3-basic-constructions/task1/task1-js.js
+++ lesson-3-basic-constructions/task1/task1-js.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var vowelCount = function(str){
   var count = 0;
   str = str.toLowerCase();
-  for(var i = 0; i < str.length; i++){
+  for(var i = 0; i < str.length -1; i++){
     if(str[i] == 'a' || str[i] == 'i' || str[i] == 'o' ||str[i] == 'e' ||str[i] == 'u' ||str[i] == 'y'){
       count+=1;
     }
--- lesson-3-basic-constructions/task2/task2-js.js
+++ lesson-3-basic-constructions/task2/task2-js.js
@@ -1,8 +1,35 @@
+'use strict';
+
 var lastName = prompt('Введите вашу фамилию');
+for (var i = 0; i < lastName.length - 1; i++) {
+  if (typeof lastName[i] !== 'string') continue;
+  lastName = prompt('Вы можете ввести только буквы');
+}
 var firstName = prompt('Введите ваше имя');
+for (var i = 0; i < firstName.length - 1; i++) {
+  if (typeof firstName[i] !== 'string') break;
+  firstName = prompt('Вы можете ввести только буквы');
+}
 var patronymic = prompt('Введите ваше отчество');
-var ageYears = prompt('Введите ваш возраст', '20');
-var gender = confirm('Ваш пол - мужской?');
+for (var i = 0; i < patronymic.length - 1; i++) {
+  if((/^\s*$/).test(patronymic)) {
+    patronymic = prompt('попробуйте ещё раз')
+  }
+  else {
+    break;
+  }
+}
+/*do while работает корректней других способов, но я не смогла это применить к строкам , пробелам и так далее, не работало
+* хотелось бы обсудить*/
+do {
+  var ageYears = prompt("Укажите ваш возраст:");
+  if (isNaN(ageYears)){
+    confirm("Введено не число. Попробуйте ещё раз.");
+  } else {
+    break;
+  }
+}while(true);
+var gender = confirm('Вы мужчина?');
 var result;
 var ageDays;
 var pension;
@@ -12,10 +39,8 @@ if (gender = true) {
 else {
   result = 'женский'
 }
-
 ageDays = ageYears * 365;
 pension = (ageYears >= 60) ? 'да': 'нет';
-
 alert('ваше ФИО: ' + lastName + ' ' + firstName + ' ' + patronymic + '\n'+
   'ваш возраст в годах: ' + ageYears + '\n' +
   'ваш возраст в днях: ' + ageDays + '\n' +
