4abfd569ebdd6863e87a1b0a5c6be2b6d18fbaf1
Liubou <alekseevna0201@gmail.com>
2018-09-17 12:09:37 +0400

    Add code for task 2

--- lesson-5-closures/task2/task2.js
+++ lesson-5-closures/task2/task2.js
@@ -3,15 +3,16 @@ let obj = {
 };
 
 function deleteClassName(obj, cls) {
-  let classArray = obj.className ? obj.className.split(' ') : [];
-  for (i = 0; i <= classArray.length -1; i++) {
-    if (classArray[i] !== cls) {
-      delete classArray.open;
-    } else {
-      return;
+  let classArray = obj.className.split(' '); //делаем массив из строки
+  for (i = 0; i <= classArray.length; i++) { //обходим массив
+    if (classArray[i] === cls) { //если какой-либо из элементов массива равен указанному склассу, то ...
+      classArray.splice(i, 1); //удаляем
+      i--; //чтобы следующее повторение цикла заново проверило i
     }
   }
-  console.log(obj.className.split(' '));
+  obj.className = classArray.join();//преобразуем вновь в строку
 }
 
 deleteClassName(obj, 'open');
+deleteClassName(obj, 'blabla');
+console.log(obj.className);
