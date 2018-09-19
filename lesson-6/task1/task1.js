a0d438888efd2d0d2df6469e9b1ca0a06c527d3a
Liubou <alekseevna0201@gmail.com>
2018-09-19 17:58:51 +0400

    Add files and code for task1

---
+++ lesson-6/task1/task1.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var billsJohn = {
+  billValue: [124, 48, 268, 180, 42]
+};
+
+var allTips = [];
+var sumWithTips = [];
+
+function tipCalculator(bill) {
+  for (var i = 0; i < billsJohn.billValue; i++)
+    var percentage;
+    if (bill < 50) {
+      percentage = 0.2;
+    } else if (bill >= 50 && bill < 200) {
+      percentage = 0.15;
+    } else {
+      percentage = 0.1;
+    }
+    return parseInt(percentage * bill);
+}
+
+allTips.push(tipCalculator(billsJohn.billValue[0]), tipCalculator(billsJohn.billValue[1]), tipCalculator(billsJohn.billValue[2]),
+  tipCalculator(billsJohn.billValue[3]), tipCalculator(billsJohn.billValue[4]));
+
+sumWithTips.push(billsJohn.billValue[0] + allTips[0], billsJohn.billValue[1] + allTips[1],
+  billsJohn.billValue[2] + allTips[2],billsJohn.billValue[3] + allTips[3],billsJohn.billValue[4] + allTips[4]);
+
+var averageTipJohn = function () {
+  return (allTips[0] + allTips[1]+ allTips[2]+ allTips[3]+ allTips[4]) / allTips.length;
+};
+
+var billsMark = {
+  billValue: [77, 375, 110, 45]
+};
+
+var tipsMark = [];
+var averageTipMark = function () {
+  return (tipsMark[0] + tipsMark[1] + tipsMark[2] + tipsMark[3]) / tipsMark.length;
+};
+
+function tipCalculatorMark(bill) {
+  for (var i = 0; i < billsMark.billValue; i++)
+    var percentage;
+    if (bill < 100) {
+      percentage = 0.2;
+    } else if (bill >= 100 && bill < 300) {
+      percentage = 0.2;
+    } else {
+      percentage = 0.25;
+    }
+    return parseInt(percentage * bill);
+}
+
+tipsMark.push(tipCalculatorMark(billsMark.billValue[0]), tipCalculatorMark(billsMark.billValue[1]),
+  tipCalculatorMark(billsMark.billValue[2]), tipCalculatorMark(billsMark.billValue[3]));
+
+if (averageTipMark() > averageTipJohn()) {
+  console.log('Family of Mark very rich and kind')
+} else {
+  console.log('Family of John very rich and kind')
+}
