c4db960370116576c96072bdd4ebf6fd98644744
Liubou <alekseevna0201@gmail.com>
2018-09-25 20:10:09 +0400

    Add first code for task1

---
+++ lesson-8-oop/task1.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+var questions = {
+  1: 'В каком году родился Виктор Цой?',
+  2: 'В каком году убили Кеннеди?',
+  3: 'На какую цифру начинается число Пи?',
+  4: 'Какой сейчас год?'
+};
+
+var allAnswers = {
+
+};
+*/
+function Question(question, answer, rightAnswer) {
+  
+}
+
+var question = new Question('В каком году родился Виктор Цой?');
