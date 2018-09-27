e81ef9206e8034cbef38f383592def7308815b4d
Liubou <alekseevna0201@gmail.com>
2018-09-27 12:45:47 +0400

    Add some code to function

--- lesson-8-oop/task1.js
+++ lesson-8-oop/task1.js
@@ -1,34 +1,36 @@
 'use strict';
 
 var mainQuestions = {
-  coi: {
+  Coi: {
     question:'В каком году родился Виктор Цой?',
     rightAnswer: '1962',
     variantsAnswer: [1962, 1963, 1965]
   },
+  Kennedy: {
+    question: 'В каком году убили Кеннеди?',
+    rightAnswer: '1963' ,
+    variantsAnswer: [1963, 1968, 1970]
+  },
   year: {
     question: 'Какой сейчас год?',
-    rightAnswer: '2018' //добавить варианты ответов
+    rightAnswer: '2018' ,
+    variantsAnswer: [2019, 2017, 2018]
   }
 };
 
 function Question(question, variantsAnswer, rightAnswer) { //функцию додумать
-  this.question = prompt(question  + variantsAnswer); //придумать как записать ответ пользователя
-  this.variantsAnswer = {
-    firstVariant: '1',
-    secondVariant: '2',
-    thirdVariant: '3'
-  };
+  this.question = prompt(question + '\n' + variantsAnswer[0]+ '\n' + variantsAnswer[1]+ '\n'  + variantsAnswer[2]);
   this.rightAnswer = rightAnswer;
   if (this.question === this.rightAnswer) {
     console.log('Это правильный ответ!')
   } else if (this.question !== this.rightAnswer) {
     console.log('Это неправильный ответ! Вы - самое слабое звено!')
-  } else {
+  } else if ((question === "") || (question == null)) {
     console.log('Введите ваш ответ корректно')
   }
 }
 
-var quiz = new Question (mainQuestions.coi.question, mainQuestions.coi.variantsAnswer, mainQuestions.coi.rightAnswer);
+var game = new Question(mainQuestions.Coi.question, mainQuestions.Coi.variantsAnswer, mainQuestions.Coi.rightAnswer);
+var gameSecond = new Question(mainQuestions.year.question, mainQuestions.year.variantsAnswer, mainQuestions.year.rightAnswer);
+var gameThird = new Question(mainQuestions.Kennedy.question, mainQuestions.Kennedy.variantsAnswer, mainQuestions.Kennedy.rightAnswer);
 
-Question();
