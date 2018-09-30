8520cdd4832a70d56157b3f56115b4e256898bad
Liubou <alekseevna0201@gmail.com>
2018-10-01 00:53:45 +0400

    Add task1, task2 and try to do tests

---
+++ lesson-9/task3/task3.test.js
@@ -0,0 +1,9 @@
+var anClean = require('./task3');
+
+describe('Function anClean', () = > {
+  it('check anagrams', () => {
+  var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
+  var array = anClean(arr);
+  expect(array).toBe([ 'ЗОВ', 'гробик', 'сектор' ]);
+  });
+});
