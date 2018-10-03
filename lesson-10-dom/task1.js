63c0083b9963663054f17fdca71a94bbaba4e0de
Liubou <alekseevna0201@gmail.com>
2018-10-03 10:33:27 +0400

    Add css, html ,js files

---
+++ lesson-10-dom/task1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+
+function THashStorage(name, alco, recipe) {
+
+    this.name = name;
+    this.alcoholic = alcoholic;
+    this.recipe = recipe;
+
+}
+var name = prompt('Введите название');
+var alco = prompt('');
+var recipe = prompt('');
+
+
+
+var Drinks = THashStorage(name, alco, recipe);
+console.log(Drinks);
