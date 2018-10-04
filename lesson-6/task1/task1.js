81c0d0703c5ec5c749b6213c3af4fde92cdce1bb
Liubou <alekseevna0201@gmail.com>
2018-10-04 15:39:42 +0400

    Transport functions in object

--- lesson-6/task1/task1.js
+++ lesson-6/task1/task1.js
@@ -3,65 +3,55 @@
 var john = {
   billValue: [124, 80, 268, 180, 42],
   tipCalculator: function (bill) {
-    this.tipsJohn = [];
-    this.sumWithTips = [];
-    for (var i = 0; i < this.billValue; i++)
-         var percentage;
-    if (bill < 50) {
-      percentage = 0.2;
-    } else if (bill >= 50 && bill < 200) {
-      percentage = 0.15;
-    } else {
-      percentage = 0.1;
+    for (var i = 0; i < this.billValue.length; i++) {
+      var percentage;
+      if (bill < 50) {
+        percentage = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+      } else {
+        percentage = 0.1;
+      }
+      return parseInt(percentage * bill);
     }
-    return parseInt(percentage * bill);
-  },
-  averageTipJohn: function () {
-    return (tipsJohn[0] + tipsJohn[1]+ tipsJohn[2]+ tipsJohn[3]+ tipsJohn[4]) / tipsJohn.length;
+
+    this.tipsJohn = [this.tipCalculator(john.billValue[0]), this.tipCalculator(john.billValue[1]),
+      this.tipCalculator(john.billValue[2]), this.tipCalculator(john.billValue[3])];
+
+    this.averageTipJohn = function () {
+      return (this.tipsJohn[0] + this.tipsJohn[1] + this.tipsJohn[2] + this.tipsJohn[3] + this.tipsJohn[4]) / this.tipsJohn.length;
+    }
   }
 };
-
-/*
-*/
-function averageTipJohn() {
-  return (tipsJohn[0] + tipsJohn[1]+ tipsJohn[2]+ tipsJohn[3]+ tipsJohn[4]) / tipsJohn.length;
-}
 
 var mark = {
   billValue: [77, 375, 110, 45],
   tipCalculatorMark: function (bill) {
-    this.tipsMark = [];
-    for (var i = 0; i < this.billValue; i++)
-         var percentage;
-    if (bill < 100) {
-      percentage = 0.2;
-    } else if (bill >= 100 && bill < 300) {
-      percentage = 0.2;
-    } else {
-      percentage = 0.25;
+    for (var i = 0; i < this.billValue.length; i++) {
+      var percentage;
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill >= 100 && bill < 300) {
+        percentage = 0.2;
+      } else {
+        percentage = 0.25;
+      }
+      return parseInt(percentage * bill);
     }
-    return parseInt(percentage * bill);
-  },
-  averageTipMark: function () {
-    tipsMark.push(tipCalculatorMark(mark.billValue[0]), tipCalculatorMark(mark.billValue[1]),
-      tipCalculatorMark(mark.billValue[2]), tipCalculatorMark(mark.billValue[3]));
-    return (this.tipsMark[0] + this.tipsMark[1] + this.tipsMark[2] + this.tipsMark[3]) / this.tipsMark.length;
+    this.tipsMark = [this.tipCalculatorMark(mark.billValue[0]), this.tipCalculatorMark(mark.billValue[1]),
+      this.tipCalculatorMark(mark.billValue[2]), this.tipCalculatorMark(mark.billValue[3])];
+
+    this.averageTipMark = function () {
+      return (this.tipsMark[0] + this.tipsMark[1] + this.tipsMark[2] + this.tipsMark[3] + this.tipsMark[4]) / this.tipsMark.length;
+    }
   }
 };
 
-/*
-tipsMark.push(tipCalculatorMark(mark.billValue[0]), tipCalculatorMark(mark.billValue[1]),
-  tipCalculatorMark(mark.billValue[2]), tipCalculatorMark(mark.billValue[3]));
-*/
-if (mark.averageTipMark() > john.averageTipJohn()) { //сравниваем средние чаевые
+john.tipCalculator();
+mark.tipCalculatorMark();
+
+if (mark.averageTipMark > john.averageTipJohn) {
   console.log('Family of Mark very rich and kind')
 } else {
   console.log('Family of John very rich and kind')
 }
-
-module.exports.averageTipsMark = averageTipMark;
-module.exports.averageTipsJohn = averageTipJohn;
-module.exports.tipCalculatorMark = tipCalculatorMark;
-module.exports.tipCalculator = tipCalculator;
-module.exports.tipsMark = tipsMark;
-module.exports.tipsJohn = tipsJohn;
