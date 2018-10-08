1471acc58c5e7d8a46b9f64d7c493991a691fa16
Liubou <alekseevna0201@gmail.com>
2018-10-08 13:40:19 +0400

    Change code task3

--- lesson-4-code-execution/task3/task3.js
+++ lesson-4-code-execution/task3/task3.js
@@ -1,27 +1,23 @@
-let array = [];
-let firstNumber = prompt('Введите первое число');
-while (true) {
-  let first = parseInt(firstNumber);
-  if (!isNaN(first) || (first === "") || (first == null)) {
-    break;
-  }
-  firstNumber = prompt('введите число правильно');
+function isNumeric(number) {
+  return !isNaN(parseFloat(number)) && isFinite(number);
 }
-let secondNumber = prompt('Введите второе число');
-while (true) {
-  let second = parseInt(secondNumber);
-  if (!isNaN(second) || (second === "") || (second == null)) {
-    break;
-  }
-  firstNumber = prompt('введите число правильно');
+
+let arr = [];
+let sum = 0;
+
+do {
+  let input = prompt('Введите суммируемое значение', '');
+  arr.push(+input);
+} while (isNumeric(input));
+
+arr.length = arr.length - 1;
+
+for (var i = 0; i < arr.length; i++) {
+  sum += arr[i];
 }
-let thirdNumber = prompt('Введите третье число');
-while (true) {
-  let third = parseInt(thirdNumber);
-  if (!isNaN(third) || (third === "") || (third == null)) {
-    break;
-  }
-  firstNumber = prompt('введите число правильно');
+
+if (arr.length === 0) {
+  alert('Вы не ввели ни одного числа.')
+} else {
+  alert('Сумма введённых вами чисел: ' + sum + '\n' + 'Вы ввели следующие числа: ' + arr);
 }
-array.push(firstNumber, secondNumber, thirdNumber);
-alert(+array[0] + +array[1] + +array[2]);
