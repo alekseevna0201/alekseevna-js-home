c5398e56f147bada4482c796d71450b0316379e6
Liubou <alekseevna0201@gmail.com>
2018-10-15 13:11:11 +0400

    Try to do correct dial

--- lesson-13-event-timer/task1.js
+++ lesson-13-event-timer/task1.js
@@ -1,18 +1,5 @@
 'use strict';
 
-// var clock = {
-//   div: {
-//     className: 'clock',
-//     style : {
-//       position: 'absolute',
-//       background: 'pink',
-//     }
-//
-//
-//   }
-// };
-
-
 var div = document.createElement('div');
 div.style.background = "-webkit-radial-gradient(black, white)";
 div.className = "clock";
@@ -47,6 +34,7 @@ hourHand.style.left ='0';
 hourHand.style.right ='0';
 hourHand.style.top ='-110px';
 hourHand.style.bottom ='0';
+hourHand.style.borderRadius ='20px';
 hourHand.zIndex ='0';
 
 center.appendChild(hourHand);
@@ -60,6 +48,7 @@ minuteHand.style.height ='180px';
 minuteHand.style.left ='0';
 minuteHand.style.top ='-140px';
 minuteHand.zIndex ='1';
+minuteHand.style.borderRadius ='20px';
 center.appendChild(minuteHand);
 
 var secondHand = document.createElement('div');
@@ -71,84 +60,90 @@ secondHand.style.height ='180px';
 secondHand.style.left ='5px';
 secondHand.style.top ='-140px';
 secondHand.zIndex ='2';
+secondHand.style.borderRadius ='20px';
 center.appendChild(secondHand);
 
 
+
 function positionDial() {
-//   for (var i =0; i <= 12; i++) {
-//     var number = document.createElement('p');
-//     number.style.background =' #A9A9A9';
-//     number.style.width ='60px';
-//     number.style.height ='60px';
-//     number.style.borderRadius ='50%';
-//     number.style.zIndex ='2';
-//     div.appendChild(number);
-//   }
-//
-//   var tagP = document.getElementsByTagName('p');
-//   var delta = Math.PI * 2 / tagP.length;
-//   var x = 0, y = 0, angle = 0;
-//
-//   for (var j = 0; j < tagP.length; j++) {
-//     tagP[i].style.left =  150 * Math.cos(angle) + 'px';
-//     tagP[i].style.top = 150 * Math.sin(angle) + 'px';
-//     angle += delta;
-//   }
-// }
-  for (var i = 0 ; i < 12; i++) {
+  for (var i = 1 ; i < 13; i++) {
 
     var number = document.createElement('p');
     number.style.position ='relative';
     number.style.background =' #A9A9A9';
     number.style.width ='60px';
     number.style.height ='60px';
-
     number.style.borderRadius ='50%';
+    number.style.textAlign ='center';
+    number.style.fontSize ='30px';
 
     number.zIndex ='2';
     div.appendChild(number);
-
+    number.appendChild(document.createTextNode(i));
 
     var centerX = center.offsetLeft + center.offsetWidth / 2;
     var centerY = center.offsetTop + center.offsetHeight / 2;
 
-    var radius = 200;
-    var angle = 30 / 180 * Math.PI;
+    var radius = 400;
+    var angle = 30;
 
     var delta = Math.PI * 2 / 12;
-    var numberCenterX = centerX + radius * Math.sin(angle);
-    var numberCenterY = centerY - radius * Math.cos(angle);
+    var numberCenterX = centerX + radius * (Math.sin(angle) / 180 * Math.PI);
+
+    var numberCenterY = centerY - radius * (Math.cos(angle) /180 * Math.PI);
 
     number.style.left = Math.round(numberCenterX - number.offsetWidth / 2) + 'px';
     number.style.top = Math.round(numberCenterY - number.offsetHeight / 2) + 'px';
-    angle+= delta;
+    angle+= 30;
   }
-
 }
-
 positionDial();
 
-window.onload = function(){
-  setInterval(fSec, 1000);
+// function seInt (){
+//   setInterval(fSec, 1000);
+//
+//   var sec = 0;
+//   var min = 0;
+//   var hour = 0;
+//
+//   function fSec(){
+//     sec = sec + 6;
+//     document.querySelector('.second').style.transform = 'rotate('+ sec + 'deg)';
+//     document.querySelector('.minute').style.transform = 'rotate('+ min + 'deg)';
+//     document.querySelector('.hour').style.transform = 'rotate('+ hour + 'deg)';
+//     document.querySelector('.second').style.transformOrigin = '100% 90%';
+//     document.querySelector('.minute').style.transformOrigin = '100% 95%';
+//     document.querySelector('.hour').style.transformOrigin = '100% 90%';
+//     if (sec + 6  === 366) {
+//       sec = 0;
+//       min = min + 6;
+//     } else if (min + 6 === 366) {
+//       min = 0;
+//       hour = hour + 6;
+//     }
+//   }
+// }
+//
+// seInt();
 
-  var sec = 0;
-  var min = 0;
-  var hour = 0;
+function timeSet() {
+  var time = new Date();
 
-  function fSec(){
-    sec = sec + 6;
-    document.querySelector('.second').style.transform = 'rotate('+sec+'deg)';
-    document.querySelector('.minute').style.transform = 'rotate('+min+'deg)';
-    document.querySelector('.hour').style.transform = 'rotate('+hour+'deg)';
-    document.querySelector('.second').style.transformOrigin = '100% 100%';
-    document.querySelector('.minute').style.transformOrigin = '100% 100%';
-    document.querySelector('.hour').style.transformOrigin = '100% 100%';
-    if (sec + 6  === 366) {
-      sec = 0;
-      min = min + 6;
-    } else if (min + 6 === 366) {
-      min = 0;
-      hour = hour + 6;
-    }
-  }
-};
+  var sec = time.getSeconds();
+  var min = time.getMinutes();
+  var hour = time.getHours();
+
+  var secondsDegrees = ((sec / 60) * 360) + 270;
+  var minuteDegrees = ((min * 60 + sec) / 3600) * 360 + 270;
+  var hourDegrees = ((hour * 3600 + min * 60 + sec) / 43200) * 360 + 270;
+
+  document.querySelector('.second').style.transformOrigin = '100% 90%';
+  document.querySelector('.minute').style.transformOrigin = '100% 95%';
+  document.querySelector('.hour').style.transformOrigin = '100% 90%';
+
+  secondHand.style.transform = 'rotate(' + secondsDegrees + 'deg)';
+  minuteHand.style.transform = 'rotate(' + minuteDegrees + 'deg)';
+  hourHand.style.transform = 'rotate(' + hourDegrees + 'deg)';
+}
+
+setInterval(timeSet, 1000);
